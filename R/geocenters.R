a564325fa9a3a2f818eb1292e0b534482fd0eff9
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2021-06-23 19:15:52 +0300

    Added testing data and its documentation

---
+++ R/geocenters.R
@@ -0,0 +1,13 @@
+#' Metadata for map ploting
+#'
+#' A dataset contains metadata, used for map ploting.
+#'
+#' @format A data table object with 11 rows and 4 variables:
+#' \describe{
+#'   \item{region}{A region, as stated in colnames or rownames in a distance matrix}
+#'   \item{y}{y coordinates for a region center}
+#'   \item{x}{x coordinates for a region center}
+#'   \item{col}{Color to use in hexadecimal format}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"geocenters"
